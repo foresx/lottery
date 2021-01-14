c263f4a288f9ff8f8a06956ada72d84d71630c0c
Genta <satioasuka@gmail.com>
2021-01-14 18:17:23 +0800

    add price.

--- js/member.js
+++ js/member.js
@@ -23,4 +23,152 @@ var member = [
     phone: "No.06",
     name: "三等奖 - 华为手表 Watch GT2",
   },
+  {
+    phone: "No.07",
+    name: "福利奖 - SKG眼部按摩仪",
+  },
+  {
+    phone: "No.08",
+    name: "福利奖 - SKG眼部按摩仪",
+  },
+  {
+    phone: "No.09",
+    name: "福利奖 - SKG眼部按摩仪",
+  },
+  {
+    phone: "No.10",
+    name: "福利奖 - SKG眼部按摩仪",
+  },
+  {
+    phone: "No.11",
+    name: "福利奖 - SKG眼部按摩仪",
+  },
+  {
+    phone: "No.12",
+    name: "福利奖 - SKG眼部按摩仪",
+  },
+  {
+    phone: "No.13",
+    name: "福利奖 - SKG眼部按摩仪",
+  },
+  {
+    phone: "No.14",
+    name: "福利奖 - SKG眼部按摩仪",
+  },
+  {
+    phone: "No.15",
+    name: "福利奖 - 网易严选腰部按摩器",
+  },
+  {
+    phone: "No.16",
+    name: "福利奖 - 网易严选腰部按摩器",
+  },
+  {
+    phone: "No.17",
+    name: "福利奖 - 网易严选腰部按摩器",
+  },
+  {
+    phone: "No.18",
+    name: "福利奖 - 网易严选腰部按摩器",
+  },
+  {
+    phone: "No.19",
+    name: "福利奖 - 网易严选腰部按摩器",
+  },
+  {
+    phone: "No.20",
+    name: "福利奖 - 网易严选EMS颈椎按摩器",
+  },
+  {
+    phone: "No.21",
+    name: "福利奖 - 网易严选EMS颈椎按摩器",
+  },
+  {
+    phone: "No.22",
+    name: "福利奖 - 网易严选EMS颈椎按摩器",
+  },
+  {
+    phone: "No.23",
+    name: "福利奖 - 网易严选EMS颈椎按摩器",
+  },
+  {
+    phone: "No.24",
+    name: "福利奖 - 网易严选EMS颈椎按摩器",
+  },
+  {
+    phone: "No.25",
+    name: "福利奖 - 网易严选EMS颈椎按摩器",
+  },
+  {
+    phone: "No.26",
+    name: "福利奖 - 网易严选EMS颈椎按摩器",
+  },
+  {
+    phone: "No.27",
+    name: "福利奖 - 网易严选EMS颈椎按摩器",
+  },
+  {
+    phone: "No.28",
+    name: "福利奖 - SKG颈椎按摩器",
+  },
+  {
+    phone: "No.29",
+    name: "福利奖 - SKG颈椎按摩器",
+  },
+  {
+    phone: "No.30",
+    name: "福利奖 - SKG颈椎按摩器",
+  },
+  {
+    phone: "No.31",
+    name: "福利奖 - SKG颈椎按摩器",
+  },
+  {
+    phone: "No.32",
+    name: "福利奖 - SKG颈椎按摩器",
+  },
+  {
+    phone: "No.33",
+    name: "福利奖 - SKG颈椎按摩器",
+  },
+  {
+    phone: "No.34",
+    name: "福利奖 - SKG颈椎按摩器",
+  },
+  {
+    phone: "No.35",
+    name: "福利奖 - SKG颈椎按摩器",
+  },
+  {
+    phone: "No.36",
+    name: "福利奖 - 网易严选无线按摩颈枕",
+  },
+  {
+    phone: "No.37",
+    name: "福利奖 - 网易严选无线按摩颈枕",
+  },
+  {
+    phone: "No.38",
+    name: "福利奖 - 网易严选无线按摩颈枕",
+  },
+  {
+    phone: "No.39",
+    name: "福利奖 - 网易严选无线按摩颈枕",
+  },
+  {
+    phone: "No.40",
+    name: "福利奖 - 网易严选无线按摩颈枕",
+  },
+  {
+    phone: "No.41",
+    name: "福利奖 - 网易严选无线按摩颈枕",
+  },
+  {
+    phone: "No.42",
+    name: "福利奖 - 网易严选无线按摩颈枕",
+  },
+  {
+    phone: "No.43",
+    name: "福利奖 - 网易严选无线按摩颈枕",
+  },
 ];
